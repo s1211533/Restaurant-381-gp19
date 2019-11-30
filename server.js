69511edb63fe0f704c781d8198b49c9935fb3204
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 00:05:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', sessionChecker, (req, res) => {
 // route for user signup
 app.route('/signup')
     .get(sessionChecker, (req, res) => {
-        res.sendFile(__dirname + '/public/signup.html');
+        res.sendFile(__dirname + '/signup.html');
     })
     .post((req, res) => {
         User.create({
@@ -82,7 +82,7 @@ app.route('/signup')
 // route for user Login
 app.route('/login')
     .get(sessionChecker, (req, res) => {
-        res.sendFile(__dirname + '/public/login.html');
+        res.sendFile(__dirname + '/login.html');
     })
     .post((req, res) => {
         var username = req.body.username,
@@ -104,7 +104,7 @@ app.route('/login')
 // route for user's dashboard
 app.get('/dashboard', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
-        res.sendFile(__dirname + '/public/dashboard.html');
+        res.sendFile(__dirname + '/dashboard.html');
     } else {
         res.redirect('/login');
     }
