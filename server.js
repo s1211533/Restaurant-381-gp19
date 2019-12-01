b18bf776b19066bed22e28c39153ffb34aa67c4d
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 15:13:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,70 +3,18 @@ const url  = require('url');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const ObjectId = require('mongodb').ObjectID;
-const mongoDBurl = 'mongodb+srv://austinmok915:97253438@restaurant-381-as8sr.gcp.mongodb.net/test?retryWrites=true&w=majority';
-const dbName = 'restaurant';
-var session = require('cookie-session');
-var express = require('express');
+const mongoDBurl = 'mongodb+srv://aaron:aaronso@aarondb-ep2mi.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'test';
 
-app = express();
 
 const server = http.createServer((req,res) => {
 	let timestamp = new Date().toISOString();
 	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
 
 	let parsedURL = url.parse(req.url,true); // true to get query as object 
-	let max = (parsedURL.query.max) ? parsedURL.query.max : 20;   		 
+	let max = (parsedURL.query.max) ? parsedURL.query.max : 20;
 
 	switch(parsedURL.pathname) {
-		case '/':
-			res.writeHead(200,{"Content-Type": "text/html"});
-			res.write('<html><head>');
-			res.write('<title>Login</title>');
-			res.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
-			res.write(' <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">');
-			res.write('</head><body>');
-        	res.write(' <header class="w3-container w3-teal">');
-			res.write('<h1>Login/Register</h1>');
-			res.write('</header>    ');     
-        	res.write(' <div class="w3-container w3-half w3-margin-top">');
-        	res.write(' <h3>Login</h3>');
-        	res.write(' <form action="/login" method="post" class="w3-container w3-card-2">');
-			res.write('	 <p>');
-			res.write(`	 <input name="logid" class="w3-input" type="text" style="width:90%" required="">`);
-			res.write('	 <label class="w3-label w3-validate">Name</label></p>');
-			res.write('	  <p>');
-			res.write(`	  <input name="password" class="w3-input" type="password" style="width:90%">`);
-			res.write('	 <label class="w3-label w3-validate">Password</label></p>');
-			res.write('	 <p>');
-			res.write(`	  <button class="w3-btn w3-section w3-teal w3-ripple"> Log in </button></p>`);
-			res.write('	 </form>	</div>   ');        
-            res.write('	<div class="w3-container w3-half w3-margin-top">');
-            res.write('      <h3>Register</h3>');
-            res.write('      <form action="/register" method="post" class="w3-container w3-card-2">');
-            res.write('         <p>');
-            res.write(`         <input name="regid" class="w3-input" type="text" style="width:90%" required="">`);
-            res.write('         <label class="w3-label w3-validate">Name</label></p>');
-            res.write('         <p>');
-            res.write(`         <input name="repassword" class="w3-input" type="password" style="width:90%">`);
-            res.write('         <label class="w3-label w3-validate">Password</label></p>');
-            res.write('         <p>');
-            res.write('         <p>');
-            res.write(`         <input name="confirmpassword" class="w3-input" type="password" style="width:90%">`);
-            res.write('         <label class="w3-label w3-validate">Confirm Password</label></p>');
-            res.write('         <p>   ');
-            res.write(`         <button class="w3-btn w3-section w3-teal w3-ripple"> Register </button></p>`);
-            res.write('      </form>');
-            res.write('</div> ');        
-			res.end('</body></html>	');
-			break;
-		case '/register':
-			login(res,parsedURL.query.criteria);
-			break;
-
-		case '/login':
-
-
-
 		case '/read':
 			read_n_print(res,parseInt(max));
 			break;
@@ -101,47 +49,8 @@ const server = http.createServer((req,res) => {
 			res.write('<html><body>');
 			res.write("404 Not Found\n");
 			res.end('<br><a href=/read?max=5>Give this a try instead?</a>');
-
 	}
 });
-
-
-
-const regsiter= (res,doc) => {
-
-
-		let docObj = {};
-		try {
-			docObj = JSON.parse(doc);
-			//console.log(Object.keys(docObj).length);
-		} catch (err) {
-			console.log(`${doc} : Invalid document!`);
-		}
-		if (Object.keys(docObj).length > 0) {  // document has at least 1 name/value pair
-			const client = new MongoClient(mongoDBurl);
-			client.connect((err) => {
-				assert.equal(null,err);
-				console.log("Connected successfully to server");
-				const db = client.db(dbName);
-				db.collection('user').insertOne(docObj,(err,result) => {
-					assert.equal(err,null);
-					res.writeHead(200, {"Content-Type": "text/html"});
-					res.write('<html><body>');
-					res.write(`Inserted ${result.insertedCount} document(s) \n`);
-					res.end('<br><a href=/>Login</a>');					
-				});
-			});
-		} else {
-			res.writeHead(404, {"Content-Type": "text/html"});
-			res.write('<html>123<body>');
-			res.write(`${doc} : Invalid document!\n`);
-			res.end('<br><a href=/>Login</a>');	
-		}
-
-
-
-}
-
 
 const findRestaurants = (db, max, criteria, callback) => {
 	//console.log(`findRestaurants(), criteria = ${JSON.stringify(criteria)}`);
@@ -164,7 +73,7 @@ const read_n_print = (res,max,criteria={}) => {
 	client.connect((err) => {
 		assert.equal(null,err);
 		console.log("Connected successfully to server");
-
+		
 		const db = client.db(dbName);
 		findRestaurants(db, max, criteria, (restaurants) => {
 			client.close();
@@ -189,7 +98,7 @@ const showdetails = (res,_id) => {
 	client.connect((err) => {
 		assert.equal(null,err);
 		console.log("Connected successfully to server");
-
+		
 		const db = client.db(dbName);
 
 		cursor = db.collection('restaurant').find({_id: ObjectId(_id)});
@@ -236,7 +145,7 @@ const insertDoc = (res,doc) => {
 		});
 	} else {
 		res.writeHead(404, {"Content-Type": "text/html"});
-		res.write('<html>123<body>');
+		res.write('<html><body>');
 		res.write(`${doc} : Invalid document!\n`);
 		res.end('<br><a href=/read?max=5>Home</a>');	
 	}
@@ -266,7 +175,7 @@ const deleteDoc = (res,criteria) => {
 		});
 	} else {
 		res.writeHead(404, {"Content-Type": "text/html"});
-		res.write('<html>5555<body>');
+		res.write('<html><body>');
 		res.write("Invalid criteria!\n");
 		res.write(criteria);
 		res.end('<br><a href=/read?max=5>Home</a>');	
@@ -295,11 +204,9 @@ const updateDoc = (res,newDoc) => {
 		});
 	} else {
 		res.writeHead(404, {"Content-Type": "text/html"});
-		res.write('<html>44444<body>');
+		res.write('<html><body>');
 		res.write("Updated failed!\n");
 		res.write(newDoc);
 		res.end('<br><a href=/read?max=5>Home</a>');	
 	}
 }
-
-server.listen(process.env.PORT || 8099);
