bac460643fb20cb5f1e8994c8550ef55ea5f5586
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 12:53:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://austinmok915:a8080881@restaurant-381-as8sr.gcp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const assert = require('assert');
+const ObjectId = require('mongodb').ObjectID;
+const url = 'mongodb+srv://austinmok915:97253438@restaurant-381-as8sr.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'restaurant';
+
+const findRestaurants = (db, callback) => {
+   let cursor = db.collection('user').find({"username": "demo" }).limit(10);
+   cursor.forEach((doc) => {
+      console.log(JSON.stringify(doc));
+   });
+   callback();
+};
+
+const client = new MongoClient(url);
+client.connect((err) => {
+   assert.equal(null,err);
+   console.log("Connected successfully to server");
+   const db = client.db(dbName);
+   findRestaurants(db,() => {
+      client.close();
+   })
