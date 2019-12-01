a8835678a6f7123ff749f999a61a0745e5ecc76e
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 11:39:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,29 @@
 const express = require('express');
-
 const session = require('cookie-session');
-
 const bodyParser = require('body-parser');
-
 const app = express();
 
-
-
 app.set('view engine','ejs');
-
-
-
 const SECRETKEY1 = 'I want to pass COMPS381F';
-
 const SECRETKEY2 = 'Keep this to yourself';
 
+const MongoClient = require('mongodb').MongoClient;
 
-
-const users = new Array(
-
-	{name: 'developer', password: 'developer'},
-
-	{name: 'guest', password: 'guest'}
-
-);
-
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://austinmok915:a8080881_B@restaurant-381-as8sr.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("restaurant").collection("user");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.set('view engine','ejs');
-
 
 
 app.use(session({
-
   name: 'session',
-
   keys: [SECRETKEY1,SECRETKEY2]
 
 }));
@@ -79,33 +67,20 @@ app.get('/login', (req,res) => {
 
 
 app.post('/login', (req,res) => {
-
 	users.forEach((user) => {
-
-		if (user.name == req.body.name && user.password == req.body.password) {
-
+		if (user.name == req.body.username && user.password == req.body.password) {
 			req.session.authenticated = true;
-
-			req.session.username = user.name;			
-
+			req.session.username = user.username;			
 		}
-
 	});
-
 	res.redirect('/');
-
 });
-
-
 
 app.get('/logout', (req,res) => {
-
 	req.session = null;
-
 	res.redirect('/');
 
 });
-
 
 
 app.listen(process.env.PORT || 8099);
