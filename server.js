c4224dbf99fdc625885253e83f92d2c98bf49e07
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 14:53:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const http = require('http');
+const cors = require('cors');
+const express = require('express');
+const fetch = require('node-fetch');
 var express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 app.get('/',function(req, res)
 	{
 		res.sendfile('login.html');
