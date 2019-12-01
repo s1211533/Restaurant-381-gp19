3d5c9b8aad865fc634ec5f3f67609778f39694bf
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 11:57:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,78 +2,30 @@ const express = require('express');
 const session = require('cookie-session');
 const bodyParser = require('body-parser');
 const app = express();
-
 const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const ObjectId = require('mongodb').ObjectID;
+const url = 'mongodb+srv://austinmok915:a8080881_B@restaurant-381-as8sr.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'restaurant';
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://austinmok915:a8080881_B@restaurant-381-as8sr.gcp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("restaurant").collection("user");
-  // perform actions on the collection object
-  client.close();
-});
+const client = new MongoClient(url);
 
+client.connect((err) => {
+   assert.equal(null,err);
+   console.log("Connected successfully to server");
 
-app.use(session({
-  name: 'session',
-  keys: [SECRETKEY1,SECRETKEY2]
-
-}));
+   const db = client.db(dbName);
 
 
 
-// support parsing of application/json type post data
+   /*
 
-app.use(bodyParser.json());
+   *  CRUD Operations
 
-// support parsing of application/x-www-form-urlencoded post data
-
-app.use(bodyParser.urlencoded({ extended: true }));
+   */
 
 
 
-app.get('/', (req,res) => {
-
-	console.log(req.session);
-
-	if (!req.session.authenticated) {
-
-		res.redirect('/login');
-
-	} else {
-
-		res.status(200).render('secrets',{name:req.session.username});
-
-	}
+   client.close();
 
 });
-
-
-
-app.get('/login', (req,res) => {
-
-	res.status(200).sendFile(__dirname + '/login.html');
-
-});
-
-
-
-app.post('/login', (req,res) => {
-	users.forEach((user) => {
-		if (user.name == req.body.username && user.password == req.body.password) {
-			req.session.authenticated = true;
-			req.session.username = user.username;			
-		}
-	});
-	res.redirect('/');
-});
-
-app.get('/logout', (req,res) => {
-	req.session = null;
-	res.redirect('/');
-
-});
-
-
-app.listen(process.env.PORT || 8099);
