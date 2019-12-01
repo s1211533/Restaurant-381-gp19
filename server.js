f946ed4a7f7a53c7974f18b130e23dfa862a63a0
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 17:26:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ const read_n_print = (res,max,criteria={}) => {
 				res.write(`<li><a href='/showdetails?_id=${r._id}'>${r.name}</a></li>`)
 			}
 			res.write('</ol>');
-			res.write('<br><a href="/insert?_id=${_id}&name=${docs[0].name}&borough=${docs[0].borough}&cuisine=${docs[0].cuisine}">Insert</a>')
+			res.write('<br><a href="/insert">Insert</a>')
 			res.end('</body></html>');
 		});
 	});
