ec04a0db69c1c2cbc856879f02ebdd55075e9f0e
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-01 14:10:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,45 +18,43 @@ const server = http.createServer((req,res) => {
 
 	switch(parsedURL.pathname) {
 		case '/':
-			res.writeHead(200,{"Content-Type": "text/html"});
-			res.write('<html><head>');
-			res.write('<title>Login</title>');
-			res.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
-			res.write(' <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">');
-			res.write('</head><body>');
-        	res.write(' <header class="w3-container w3-teal">');
-			res.write('<h1>Login/Register</h1>');
-			res.write('</header>    ');     
-        	res.write(' <div class="w3-container w3-half w3-margin-top">');
-        	res.write(' <h3>Login</h3>');
-        	res.write(' <form action="/login" method="post" class="w3-container w3-card-2">');
-			res.write('	 <p>');
-			res.write(`	 <input name="logid" class="w3-input" type="text" style="width:90%" required="">`);
-			res.write('	 <label class="w3-label w3-validate">Name</label></p>');
-			res.write('	  <p>');
-			res.write(`	  <input name="password" class="w3-input" type="password" style="width:90%">`);
-			res.write('	 <label class="w3-label w3-validate">Password</label></p>');
-			res.write('	 <p>');
-			res.write(`	  <button class="w3-btn w3-section w3-teal w3-ripple"> Log in </button></p>`);
-			res.write('	 </form>	</div>   ');        
-            res.write('	<div class="w3-container w3-half w3-margin-top">');
-            res.write('      <h3>Register</h3>');
-            res.write('      <form action="/register" method="post" class="w3-container w3-card-2">');
-            res.write('         <p>');
-            res.write(`         <input name="regid" class="w3-input" type="text" style="width:90%" required="">`);
-            res.write('         <label class="w3-label w3-validate">Name</label></p>');
-            res.write('         <p>');
-            res.write(`         <input name="repassword" class="w3-input" type="password" style="width:90%">`);
-            res.write('         <label class="w3-label w3-validate">Password</label></p>');
-            res.write('         <p>');
-            res.write('         <p>');
-            res.write(`         <input name="confirmpassword" class="w3-input" type="password" style="width:90%">`);
-            res.write('         <label class="w3-label w3-validate">Confirm Password</label></p>');
-            res.write('         <p>   ');
-            res.write(`         <button class="w3-btn w3-section w3-teal w3-ripple"> Register </button></p>`);
-            res.write('      </form>');
-            res.write('</div> ');        
-			res.end('</body></html>	');
+			app.get('/', (req,res) => {
+				console.log(req.session);
+				if (!req.session.authenticated) {
+					res.redirect('/login');
+				} else {
+					res.status(200).render('secrets',{name:req.session.username});
+				}
+
+});
+
+
+
+app.get('/login', (req,res) => {
+
+	res.status(200).sendFile(__dirname + '/public/login.html');
+
+});
+
+
+
+app.post('/login', (req,res) => {
+
+	users.forEach((user) => {
+
+		if (user.name == req.body.name && user.password == req.body.password) {
+
+			req.session.authenticated = true;
+
+			req.session.username = user.name;			
+
+		}
+
+	});
+
+	res.redirect('/');
+
+});
 			break;
 		
 		default:
