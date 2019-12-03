dbc965fdcb1f7ba1eb14de89d107791710eca553
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-04 00:17:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/list',(req, res) => {
 			const db = client.db(dbName);
 			const findRestaurant = (db, callback) => { 
 				let cursor = db.collection('restaurants').find() 
-				res.status(200).render('restaurantList',{name:req.session.username},
+				res.status(200).render('restaurantList',{uname:req.session.username},
 						       {resname:cursor});
 				callback();
 			}
